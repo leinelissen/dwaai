1172b0ba3d569af1ffdc4aa7685e33b2604bbf63
Jordy-A <jordyalblas@live.nl>
2020-01-07 12:14:01 +0100

    Backend

---
+++ webapp/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const multer  = require('multer');
+const upload = multer();
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// routes
+app.post('/recording', upload.single('audio'), function (req, res) {
+  console.log(req.file);
+  res.send({ express: 'ok' });
+});
